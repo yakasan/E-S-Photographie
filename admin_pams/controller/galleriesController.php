063499cb88321fdcbe830c5ac718ea5ba4195c1a
yakasan <mehdichilla33@gmail.com>
2014-12-02 14:46:12 +0100

    hjgt

--- admin_pams/controller/galleriesController.php
+++ admin_pams/controller/galleriesController.php
@@ -2,15 +2,7 @@
 include("../view/galleries.tpl");
 require_once("../model/class.model.php")
 
-function show_title_galleries(){
-	$connexion = new PDO('mysql:host=localhost; dbname=es_photographie', 'stagiaire', 'stagiaire');
-	$req=$connexion->query("SELECT * FROM galleries");
-	while($row = $req->fetch())
-	{
-			echo "<a href='#' class='list-group-item'> ".$row['title']." </a>";
-	}
-	$req->closeCursor();
-}
+
 
 
 ?>
--- admin_pams/model/class.model.php
+++ admin_pams/model/class.model.php
@@ -2,18 +2,23 @@
 
 class Model {
     public static $dns = "mysql:host=localhost; dbname=es_photographie";
-    public static $user = "Stagiaire";
-    public static $pass = "Stagiaire";
+    public static $user = "stagiaire";
+    public static $pass = "stagiaire";
     $connexion = new PDO($dns, $user, $pass);
+}/*
+function joinGalleries();{
+
+       
+
+   $query = self::$connexion->prepare("SELECT j.nom nom_jeu, p.prenom prenom_proprietaire FROM proprietaires p INNER JOIN photos j
+    ON j.ID_proprietaire = p.ID;
+}*/
+function show_title_galleries(){
+    $req=$connexion->query("SELECT * FROM galleries");
+    while($row = $req->fetch())
+    {
+            echo "<a href='#' class='list-group-item'> ".$row['title']." </a>";
+    }
+    $req->closeCursor();
 }
-// model pour jointure de tables, a conserver
-    /*public $hasAndBelongsToMany = array(
-        'User' =>
-            array(
-                'className' => 'User',
-                'joinTable' => 'games_users',
-                'foreignKey' => 'game_id',
-                'associationForeignKey' => 'user_id'      
-           )
-    );*/
 ?>
