1a03024e6a11326ab121bbca66d645fcd9cf2d9b
sSeTOo <sylvain.lavayssiere@gmail.com>
2014-12-02 14:53:17 +0100

    suppr fonction du gallerie controller

--- admin_pams/controller/galleriesController.php
+++ admin_pams/controller/galleriesController.php
@@ -2,18 +2,5 @@
 include("../view/galleries.tpl");
 require_once("../model/class.model.php");
 
-<<<<<<< HEAD
-function show_title_galleries(){
-	$req=$connexion->query("SELECT * FROM galleries");
-	while($row = $req->fetch())
-	{
-			echo "<a href='#' class='list-group-item'> ".$row['title']." </a>";
-	}
-	$req->closeCursor();
-}
-=======
-
->>>>>>> 063499cb88321fdcbe830c5ac718ea5ba4195c1a
-
 
 ?>
