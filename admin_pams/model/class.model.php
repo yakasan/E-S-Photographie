b04e83667cdf37d79ee8e554c6d8a011aa386226
yakasan <mehdichilla33@gmail.com>
2014-12-03 15:09:40 +0100

    modif

--- admin_pams/model/class.model.php
+++ admin_pams/model/class.model.php
@@ -8,24 +8,9 @@ class Model {
 
     public static $connexion ;
 
-<<<<<<< HEAD
-   $query = self::$connexion->prepare("SELECT j.nom nom_jeu, p.prenom prenom_proprietaire FROM proprietaires p INNER JOIN photos j
-    ON j.ID_proprietaire = p.ID;
-}*/
-/*function show_title_galleries(){
-    $req=$connexion->query("SELECT * FROM galleries");
-    while($row = $req->fetch())
-    {
-            echo "<a href='#' class='list-group-item'> ".$row['title']." </a>";
-    }
-    $req->closeCursor();
-}*/
-?>
-=======
+
     function __construct() {
         $this->connexion = new PDO($dns, $user, $pass);
     }   
 }
 ?>
-
->>>>>>> 03bc2887d234ac7248004b059dc647116b9d683c
