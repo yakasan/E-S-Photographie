cbeb3655bf9ae41a9bd250ce70ac1cd34a9b15a8
CaptainHD <CvJDV.63Zg>
2014-12-04 09:23:22 +0100

    modif photocontroller

--- admin_pams/controller/photosController.php
+++ admin_pams/controller/photosController.php
@@ -6,7 +6,9 @@ include('../model/class.photo.php');
 		//print_r($_POST['addPhoto']);
 		//die();
 		$nouvellePhoto = new Photo();
-		$nouvellePhoto->AddPhoto(' ', $_POST['addPhoto']['title'], $_POST['addPhoto']['desc'], ' ', ' ');
+		print_r($nouvellePhoto);
+		die;
+		//$nouvellePhoto->AddPhoto(' ', $_POST['addPhoto']['title'], $_POST['addPhoto']['desc'], ' ', ' ');
 	}
 
 include("../view/photos.tpl")
