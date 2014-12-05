38493efd55cbed42d6f0b6e497f0e4f0fb8f68a5
sSeTOo <sylvain.lavayssiere@gmail.com>
2014-12-05 16:10:13 +0100

    détail du code

--- admin_pams/controller/galleriesController.php
+++ admin_pams/controller/galleriesController.php
@@ -3,6 +3,7 @@ require_once('indexx.php');
 require_once("model/class.gallery.php");
 
 function createGalleryFolder ($title){
+	//traitement de la chaine de caractères pour avoir un nom formaté
 	$title=strtolower($title);
 	$title=str_replace(" ","_",$title);
 
@@ -17,97 +18,85 @@ function createGalleryFolder ($title){
 }
 
 function destructFolder ($nameGallery){
-
+	//traitement de la chaine de caractères 
 	$nameGallery = strtolower($nameGallery);
 	$nameGallery = str_replace(" ","_",$nameGallery);
 	$nameGallery = str_replace(".", "", $nameGallery);
-	print_r($nameGallery);
 
+	//ouverture du dossier choisi 
 	$selectGallery = opendir("../gallery/$nameGallery");
+	//lecture de l'ensemble des fichiers dans le dossier
     while (false !== ($nameFile = readdir($selectGallery))) {
         if ($nameFile != "." && $nameFile != "..") {
             echo "$nameFile a été supprimé";
+            //suppression des fichiers
             unlink("../gallery/$nameGallery/$nameFile");
         }
     }
+    //suppression du dossier une fois vidé
     echo"la galerie a été supprimée";
 	rmdir("../gallery/$nameGallery");
 }
 
-
+// Appel du model en cas de connexion en tant que admin
 if (isset($_SESSION['login']) && $_SESSION['admin']==1){
 	$gallery = new Gallery();
 
-
-if(empty($_POST['nameGallery']) && empty($_POST['textGallery'])){
-	$_SESSION['dataLess']="infos manquantes";
-}/*else{
-	unset($_SESSION['dataLess']);
-}*/
-
+// CREATION DE LA GALERIE
+// Récupération des données de formulaire envoyé en post 
 if(!empty($_POST['nameGallery']) && !empty($_POST['textGallery'])){
 		$title=$_POST['nameGallery'];
 		$description=$_POST['textGallery'];
 
+	//On cherche a voir si le nom existe déjà dans la BDD
 		$exist = $gallery->knowFolderExist($title);
 
 		if ($exist){
 			$_SESSION['galleryExist']="Galerie déjà existante";
 		}else{
+			//On lance la création dans la base de données
 			$gallery->createGallery($title, $description);
+			//On lance la création du répertoir 
 			createGalleryFolder($title);
 		}
-
+		//On remet le $_POST à zero en lui donnant une valeur null
 		$_POST='';
 }
+
+//On récupère l'ensemble des infos de la BDD dans une variable pour l'exploiter dans le controller
 $tableData = $gallery->getData();
 
-
-	if(empty($_POST['nameGallery']) && empty($_POST['textGallery'])){
-		$_SESSION['dataLess']="infos manquantes";
-	}
-
-	if(!empty($_POST['nameGallery']) && !empty($_POST['textGallery'])){
-			$title=$_POST['nameGallery'];
-			$description=$_POST['textGallery'];
-
-			$exist = $gallery->knowFolderExist($title);
-
-			if ($exist){
-				$_SESSION['galleryExist']="Galerie déjà existante";
-			}else{
-				$gallery->createGallery($title, $description);
-				createGalleryFolder($title);
-			}
-
-			$_POST='';
-			//unset($_SESSION['galleryExist']);
-	}
-	$tableData = $gallery->getData();
-
+	// je clique sur le bouton MODIFIER dont la valeur est « modifGallery »
 	if (isset($_POST['modifGallery'])&& isset($_POST['gallerieName'])) {
-	 	// j'ai cliqué sur « modifGallery »
-		 	print_r($_POST['modifGallery']);
-		 	print_r($_POST['gallerieName']);
+			//récupération des données en variable de session
 		 	$_SESSION['gallerieName'] = $_POST['gallerieName'];
+		 	//redirection vers le controller concerné
 		 	header('Location:indexx.php?page=8');
 
+ 	// j'ai cliqué sur « uploadPicture »
 	} elseif (isset($_POST['uploadPicture'])&& isset($_POST['gallerieName'])) {
-	    // j'ai cliqué sur « uploadPicture »
 	    	$nameGallery = $_POST['gallerieName'];
+	    	//récupération du nom en variable de session
 	    	$_SESSION['gallerieNameUpload'] = $_POST['gallerieName'];
+	    	//récupération de l'id en variable de session et ré
 	    	$_SESSION['gallerieIdUpload'] = $gallery->getIdGallery($nameGallery);
 	    	header('Location:indexx.php?page=4');
 
+	// j'ai cliqué sur « supprGallery »
 	} elseif (isset($_POST['supprGallery'])&& isset($_POST['gallerieName'])) {
-	    // j'ai cliqué sur « supprGallery »
+			//récupération du POST en variable
 	    	$gallerieName=$_POST['gallerieName'];
+	    	//appel de la fonction pour supprimer le répertoir
 			destructFolder($gallerieName);
+			//appel de la methode pour supprimer l'entrée dans la BDD
 			$gallery->destructGallery($gallerieName);
+			//redirection vers le controller concerné
 			header('Location:indexx.php?page=2');
 	}
+	// inclusion de la vue à la fin pour l'affichage de l'ensemble des variables après traitement
 	include("view/galleries.tpl");
 }else{
+	//si la personne n'est pas connectée on le redirige vers l'index du site à la racine
 	header("Location: ../index.html");
 	die;
 }
