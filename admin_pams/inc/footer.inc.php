77fcb8612464ca20373ab7cf8ad5e312fa143bbd
sSeTOo <sylvain.lavayssiere@gmail.com>
2014-12-05 16:52:52 +0100

    ajout copyright

--- admin_pams/inc/footer.inc.php
+++ admin_pams/inc/footer.inc.php
@@ -1,6 +1,7 @@
 <footer class="footer clearfix">
 	<div class="container">
         <p class="text-muted">Création des développeurs anonymes</p>
+        <p class="text-muted">© Sylvain Lavayssiere / Alexis Ficheux / Jerome Richon / Patrice Sureaud / Mehdi Chilla</p>
   	</div>
 </footer>
 </body>
